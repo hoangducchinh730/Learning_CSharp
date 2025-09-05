6dab564ad6feca5f61514fb503913f1833e5ba25
Hoang Duc Chinh <hoangducchinh730@gmail.com>
2025-09-05 14:52:37 +0700

    Initial commit

---
+++ MiniProject/ToDoList/Calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+Console.WriteLine("Hello.");
+
+Console.Write("Input the first number: ");
+var firstNumber = double.Parse(Console.ReadLine());
+
+Console.Write("Input the second number: ");
+var secondNumber = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[A]dd numbers.");
+Console.WriteLine("[S]ubstract numbers.");
+Console.WriteLine("[M]ultiply numbers.");
+
+var userChoice = Console.ReadLine().ToUpper();
+
+if (EqualsCaseInsensitive(userChoice, "A"))
+{
+    var sum = firstNumber + secondNumber;
+    PrintFinalResult(firstNumber, secondNumber, sum, "+");
+}
+else if (EqualsCaseInsensitive(userChoice, "S"))
+{
+    var difference = firstNumber + secondNumber;
+    PrintFinalResult(firstNumber, secondNumber, difference, "-");
+}
+else if (EqualsCaseInsensitive(userChoice, "M"))
+{
+    var product = firstNumber * secondNumber;
+    PrintFinalResult(firstNumber, secondNumber, product, "*");
+}
+else
+{
+    Console.WriteLine("Invalid choice.");
+}
+
+
+Console.WriteLine("Press any key to close.");
+Console.ReadKey();
+
+void PrintFinalResult(double firstNumber, double secondNumber, double result, string @operator)
+{
+    Console.WriteLine($"{firstNumber} {@operator} {secondNumber} = {result}");
+}
+
+bool EqualsCaseInsensitive(string left, string right)
+{
+    return left.ToUpper() == right.ToUpper();
+}
---
+++ MiniProject/ToDoList/ToDoList/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+Console.WriteLine("Hello!\n");
+
+List<string> list = new List<string>();
+
+do
+{
+    Console.WriteLine("What do you want to do?");
+    Console.WriteLine("[S]ee all TODOs");
+    Console.WriteLine("[A]dd a TODO");
+    Console.WriteLine("[R]emove a TODO");
+    Console.WriteLine("[E]xit");
+
+    string? userChoice = Console.ReadLine();
+
+    userChoice = userChoice?.ToUpper();
+
+    switch (userChoice)
+    {
+        case "S":
+            if(list.Count == 0)
+                ShowMessage("No TODOs have been added yet.");
+            else 
+                ShowAllTodo();
+            break;
+        case "A":
+            while (true)
+            {
+                Console.WriteLine("Enter the TODO description: ");
+                string? todo = Console.ReadLine();
+                if (string.IsNullOrEmpty(todo))
+                    Console.WriteLine("The description cannot be empty.");
+                else if (list.Contains(todo.ToUpper()) == true)
+                    Console.WriteLine("The description must be unique.");
+                else
+                {
+                    list.Add(todo.ToUpper());
+                    ShowMessage($"TODO successfully added: {todo.ToUpper()}");
+                    break;
+                }
+            }
+            break;
+        case "R":
+            while (true)
+            {
+                if (list.Count == 0)
+                {
+                    ShowMessage("No TODOs have been added yet.");
+                    break;
+                }
+                ShowMessage("Select the index of the TODO you want to remove:");
+                ShowAllTodo();
+                string? input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                    Console.WriteLine("Selected index cannot be empty.");
+                else
+                {
+                    var index = int.Parse(input);
+                    if (index < 1 || index > list.Count)
+                        Console.WriteLine("The given index is not valid.");
+                    else
+                    {
+                        string removedTodo = list[index - 1];
+                        list.RemoveAt(index - 1);
+                        ShowMessage($"TODO removed: {removedTodo}");
+                        break;
+                    }
+                }
+            }
+            break;
+        case "E":
+            return;
+        default:
+            ShowMessage("Incorrect input");
+            break;
+    }
+    Console.WriteLine();
+} while (true);
+
+
+void ShowMessage(string message)
+{
+    Console.WriteLine(message);
+}
+
+void ShowAllTodo()
+{
+    for (int i = 0; i < list.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {list[i].ToUpper()}");
+    }
+}
